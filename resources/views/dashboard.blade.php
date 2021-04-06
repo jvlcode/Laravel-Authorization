5b39dbc6da654b40effeceb39ec58dd869b00953
jvlogesh <72685700+jvlogesh@users.noreply.github.com>
2021-04-06 17:54:42 +0530

    code

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    You're logged in!
+                </div>
+                <div class="m-6 space-x-3 text-center space-y-3">
+                    @can('isAdmin')
+                    <a href="{{url('admin-control-panel')}}" class="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-blue-700 rounded shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
+                    >
+                      Admin Control Panel
+                    </a>
+                    @endcan
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
